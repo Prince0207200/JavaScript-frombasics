c9ba1e4f50461de936b1db559482e68d2adf2feb
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2024-04-20 06:00:45 +0000

    string operation

--- 02_dataTypes/second.js
+++ 02_dataTypes/second.js
@@ -1,6 +1,13 @@
-let score="33a";
-console.log(typeof score);
-console.log(score);
-let num=Number(score);
-console.log(typeof num);
-console.log(num);
+// let score="33a";
+// console.log(typeof score);
+// console.log(score);
+// let num=Number(score);
+// console.log(typeof num);
+// console.log(num);
+// let str="Alpha"
+// console.log(`This is backtic ${score} is core and ${str} is str`);
+const a=new String("alpha-ram-aklvya-don");
+console.log(a.split("-"));
+console.log(a.includes("do"));
+console.log(a.replace("-","@"));
+console.log(a.replaceAll("-","@"));
