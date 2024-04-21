9968e6665fb1cb53abaeb0f4ebb5b9c371beba58
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2024-04-21 06:24:04 +0000

    jason

---
+++ 02_basics/objDeStruct.js
@@ -0,0 +1,19 @@
+const course={
+    cousename: "Alpha",
+    id: 32,
+    courseInstructor: "Prince"
+}
+const {courseInstructor:c}= course
+console.log(c)
+
+
+////// jason API format
+// {
+//     "sd": "Asdf",
+//     "gf": 32
+// }
+
+// [
+//     {},
+//     {}
+// ]
