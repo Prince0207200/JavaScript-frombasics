4f346a603f9fed6479541207ae888af7b15e1ba5
Prince Gupta <121662994+Prince0207200@users.noreply.github.com>
2024-04-21 08:06:47 +0000

    fun

---
+++ 02_basics/function.js
@@ -0,0 +1,14 @@
+// function calculatePrice (...num1){
+//     return num1;
+// }
+// console.log(calculatePrice(6,3,4,5,6,7));
+
+const user={
+    name: "supla",
+    class: 12,
+    id: 1
+};
+function finf(user){
+    console.log(`name is ${user.name} and class is ${user.class} f`);
+};
+finf();
